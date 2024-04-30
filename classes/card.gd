481819489163f8dbf2a6c287c8c0278f02b3c35a
TimYLSo <52621698+TimYLSo@users.noreply.github.com>
2024-04-30 23:15:23 +1200

    gave shop cards

--- classes/card.gd
+++ classes/card.gd
@@ -1,14 +1,22 @@
 extends Node
 class_name card
-enum card_type {action_card,static_card,event_card}
+enum CardType {action_card,static_card,event_card}
+enum Rarity {common,uncommon,rare,mythic}
+var card_type:CardType
 var card_name:String
 var card_description:String
-enum rarity {common,uncommon,rare}
+var rarity:Rarity
 var cost:int
 var card_id:int
 var image_path:String
+var in_shop:bool
 
-
-func __init(n:String):
-	self.card_name = n
-
+func _init(info:Dictionary):
+	self.card_name = info.get("card_name")
+	card_type = info.get("card_type")
+	card_description = info.get("card_description")
+	image_path = info.get("image_path")
+	rarity = info.get("rarity")
+	cost = info.get("cost")
+	in_shop = info.get("in_shop")
+	card_id = info.get("card_id")
--- scenes/shop/shop_card.gd
+++ scenes/shop/shop_card.gd
@@ -1,52 +1,46 @@
 extends Node2D
-
+class_name shop_card
 @onready var border_color = $border 
 @onready var buy_button = $area2d/buy_button
+@onready var name_box = $color/card_name
+@onready var cost_box = $color/card_cost
+@onready var description_box = $color/ColorRect/card_description
+var card_displayed:card
+var card_name:String = "card_name"
+var cost:int = 2
+var description:String ="description"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	border_color.hide()
 	buy_button.hide()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	change_display()
 
 
-func _on_color_mouse_exited():
-	border_color.hide()
+func __init(c:card):
+	card_displayed = c
 
-
-func _on_color_mouse_entered():
+func buy():
+	$area2d.hide()
+	$color.hide()
 	border_color.show()
+	
+func update_card_info(c:card):
+	card_name= c.card_name
+	description = c.card_description
+	cost = c.cost
+
+func change_display():
+	description_box.clear()
+	description_box.add_text(description)
+	name_box.text = card_name
+	cost_box.text = str(cost)
 
 
-
-
-
-
-
-
-
-func _on_area_2d_input_event(viewport:Node, event:InputEvent, shape_idx:int):
-	if event is InputEventMouseButton and event.pressed:
-		print("Mouse button pressed")
-
-
-
-
-
-
-
-
-
-
-func _on_area_2d_mouse_entered():
-	print("entered")  
+func _on_area_2d_mouse_entered(): 
 	border_color.show()
 	buy_button.show()
 
-func _on_area_2d_mouse_exited():
-	print("exited")  
+func _on_area_2d_mouse_exited(): 
 	border_color.hide()
 	buy_button.hide()
 
