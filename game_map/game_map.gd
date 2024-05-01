c5566216088df2ea8342c9c27caff64cc478cd27
TimYLSo <52621698+TimYLSo@users.noreply.github.com>
2024-05-02 03:04:41 +1200

    Fixed UI
    Fixed UI by making the UI elements in place

--- game_map/game_map.gd
+++ game_map/game_map.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 var number_of_survivors:int = 8
-
+@onready var UI_layer  = $UI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	get_node("SurvivorHandler").show_survivor_panel.connect(_on_survivor_handler_show_survivor_panel)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,3 +17,8 @@ func set_number_of_survivors(amount:int):
 func get_number_of_survivors():
 	
 	return self.number_of_survivors
+
+
+func _on_survivor_handler_show_survivor_panel(s:Survivor):
+	print("signal recieved")
+	UI_layer.display_survivor_info(s)
