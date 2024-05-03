1ca952ae80101d5493b3b59807728592a2ba94b4
kiwilu <waynelu1121@gmail.com>
2024-05-03 16:51:50 +1200

    daynightcycle

--- classes/daynightcycle.gd
+++ classes/daynightcycle.gd
@@ -1,11 +1,38 @@
-extends Node
+extends Node 
+class_name DayNightCycle
+
+signal day_ends
+
+var time_passed = 0
+var one_day_duration = 60.0  
+var is_night = false
+var dayingame=0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time_passed += delta
+	if time_passed >= one_day_duration:
+		time_passed = 0
+		dayingame += 1
+		entershop()
+	
+func timer():
+	return one_day_duration-time_passed
+
+func get_dayingame():
+	return dayingame
+
+func get_dayornight():
+	return is_night
+
+func totalgametime():
+	return dayingame+"m"+time_passed+"s"
+
+func entershop():
+	day_ends.emit()
+
