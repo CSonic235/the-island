e0e2ff38f0bbf18f9ddaa2d56fe3e5f74d96ea27
TimYLSo <52621698+TimYLSo@users.noreply.github.com>
2024-05-16 06:08:11 +1200

    added a test

--- tests/unit/test_play_cards.gd
+++ tests/unit/test_play_cards.gd
@@ -13,19 +13,35 @@ func before_all():
 			var card_to_add = card.new(c)
 			for i in 4 - rarity:
 				_possible_cards.append(card_to_add)
+
 func before_each():
 	var deck_size = 10
 	var _cards_in_deck = []
 	for i in range(deck_size):
-		_cards_in_deck.append( _possible_cards[0])
+		_cards_in_deck.append( _possible_cards[i])
 	_play_area = play_cards.new(_cards_in_deck)
 func test_draw_a_card():
+	## checks that drawing a card puts a card from the deck into the hand
 	_play_area.draw()
 	assert_eq(_play_area.hand.size(), 1, "one card in hand")
 	assert_eq(_play_area.cards_in_deck.size(), 9, "9 cards in deck")
 	assert_eq(_play_area.discard_pile.size(), 0, "0 cards in discard")
 
 func test_draw_past_hand_size():
-	pass
+	## checks that drawing a card when hand size is at max puts it in the discard pile
+	var hand_size:int = 7
+	for x in range(hand_size):
+		_play_area.draw()
+	_play_area.draw()
+	assert_eq(_play_area.hand.size(), 7, "7 cards in hand")
+	assert_eq(_play_area.cards_in_deck.size(), 2, "2 cards in deck")
+	assert_eq(_play_area.discard_pile.size(), 1, "1 cards in discard")
 func test_draw_from_empty_deck():
-	pass
+	 ## checks that drawing from an empty deck shuffle the discard pile back into the deck
+	var deck_size:int = 10 
+	for x in range(deck_size):
+		_play_area.draw()
+	_play_area.draw()
+	assert_eq(_play_area.hand.size(), 7, "7 cards in hand")
+	assert_eq(_play_area.cards_in_deck.size(), 2, "2 cards in deck")
+	assert_eq(_play_area.discard_pile.size(), 1, "1 cards in discard")
