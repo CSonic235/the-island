0d7a100540280af7af4f87deee56f76624df737f
kiwilu <waynelu1121@gmail.com>
2024-05-16 14:00:46 +1200

    signal update

--- scripts/handler/CardAttributeCounter.gd
+++ scripts/handler/CardAttributeCounter.gd
@@ -1,22 +1,28 @@
 extends Node
 
-signal static_attributes_counted(cold_count: int, wet_count: int)
+signal static_attributes_counted(attributes_count: Dictionary)
 
 func count_static_attributes(cards: Array[Card], card_id_to_count: int):
-	var cold_count = 0
-	var wet_count = 0
-	var
-	
+	var attributes_count = {
+		"cold": 0,
+		"wet": 0,
+		"hot": 0,
+		"windy": 0,
+		"humid": 0,
+		"dry": 0,
+		"fish": 0,
+		"fertile_soil": 0,
+		"infertile_soil": 0
+	}
+
 	for card in cards:
 		if card.card_id == card_id_to_count:
 			var static_attributes = card.static_attributes
 			if static_attributes and "attributes" in static_attributes:
 				var attributes = static_attributes["attributes"]
 				for attribute in attributes:
-					if attribute == "cold":
-						cold_count += 1
-					elif attribute == "wet":
-						wet_count += 1
+					if attribute in attributes_count:
+						attributes_count[attribute] += 1
 
-	emit_signal("static_attributes_counted", cold_count, wet_count)
+	emit_signal("static_attributes_counted", attributes_count)
 
